fd061f58c24e86c179f232df24e5b77a14cd4122
Andy Stock <andy.stock@niva-dk.dk>
2026-09-20 09:25:43 +0200

    New function to load Ecospace MPAs.

--- R/read_xml_ecospace.R
+++ R/read_xml_ecospace.R
@@ -1,5 +1,3 @@
-xmldoc=paste0(system.file('extdata', package = 'ecocx'),"/anchovy_bay_ecosim_ex.eiixml")
-
 #' Load Ecospace scenario information from EIIXML
 #' @param xmldoc XML2 document.
 #' @returns Data frame with Ecospace scenario names and IDs.
@@ -67,14 +65,57 @@ get_ecospace_basemap=function(xmldoc,scenario_name)
   map
 }
 
-#' #' Load static environmental driver maps from EIIXML
+
+#' Parse map data string from Ecospace EIIXML
+#'
+#' @param mapdata String with map data from EIICML. May be plain or compressed.
+#' @param basemap The basemap (0 in water cells/the study area, NA elsewhere).
+#' @param mpa Boolean: is this an MPA map? EwE stores MPA maps are stored in a different format.
+#'
+#' @returns Matrix with the map data.
+extract_map_data=function(mapdata,basemap, mpa=F)
+{
+  m=basemap$values
+  if(!startsWith(mapdata,"gzip") & !grepl("[0-9]", mapdata)) {  #not compressed, no numbers
+    warning("Map contained no data. Returning NULL.")
+    return(NULL)
+  } else if(startsWith(mapdata,"gzip")) {  #compressed
+    str_map=decode_ewe_map(mapdata,nodata=0) #MPA: has specific format
+    if(mpa) {
+      for(j in 1:nrow(m)) {
+        ix=which(!is.na(m[j,]))
+        if(length(ix)>0 & length(str_map[[j]]>0)) {
+          str_map[[j]][is.na(str_map[[j]])]=0
+          m[j,ix]=m[j,ix]+str_map[[j]]}
+      }
+    } else { #other maps (not MPAs)
+      for(j in 1:nrow(m)) {
+        ix=which(!is.na(m[j,]))
+        if(length(ix)>0) {m[j,ix]=str_map[[j]]}
+      }
+    }
+  } else {
+    str_map=unlist(strsplit(mapdata,split=";"))
+    for(j in 1:nrow(m))
+    {
+      ix=which(!is.na(m[j,]))
+      if(length(ix)>0) {
+        values=as.numeric(unlist(strsplit(str_map[j],split=" ")))
+        m[j,ix]=values
+      }
+    }
+  }
+  m
+}
+
+#' #' Load static environmental maps from EIIXML
 #'
 #' @param xmldoc XML2 document.
 #' @param scenario_name Name of the scenario.
 #'
-#' @returns List of \code{EcospaceMap} objects, each containing data about one environmental driver.
+#' @returns List of \code{EcospaceMap} objects, each containing one environmental map.
 #' @export
-get_ecospace_envdrivers=function(xmldoc,scenario_name)
+get_ecospace_envmaps=function(xmldoc,scenario_name)
 {
   #load basemap as only data for in-AOI cells are stored
   basemap=get_ecospace_basemap(xmldoc, scenario_name)
@@ -96,36 +137,89 @@ get_ecospace_envdrivers=function(xmldoc,scenario_name)
     driver=list()
     driver$name=df$LayerName[i]
     driver$id=df$LayerID[i]
-    #load spatial data
-    m=basemap$values
-    if(startsWith(df$LayerMAP[i],"gzip")) {
-      str_map=decode_ewe_map(df$LayerMAP[i],nodata=0)
-      for(j in 1:nrow(m)) {
-        ix=which(!is.na(m[j,]))
-        if(length(ix)>0) {m[j,ix]=str_map[[j]]}
-      }
-    } else {
-      str_map=unlist(strsplit(df$LayerMAP[i],split=";"))
-      for(j in 1:nrow(m))
-      {
-        ix=which(!is.na(m[j,]))
-        if(length(ix)>0) {
-          values=as.numeric(unlist(strsplit(str_map[j],split=" ")))
-          m[j,ix]=values
-        }
-      }
-    }
-    driver$values=m
+    driver$values=extract_map_data(df$LayerMAP[i],basemap)
     class(driver)="EcospaceMap"
     l_drivers[[driver$name]]=driver
   }
   l_drivers
 }
 
+#' #' Load static habitat maps from EIIXML
+#'
+#' @param xmldoc XML2 document.
+#' @param scenario_name Name of the scenario.
+#'
+#' @returns List of \code{EcospaceMap} objects, each containing one habitat map.
+#' @export
+get_ecospace_habmaps=function(xmldoc,scenario_name)
+{
+  #load basemap as only data for in-AOI cells are stored
+  basemap=get_ecospace_basemap(xmldoc, scenario_name)
+
+  #load scenario information
+  scenarios=get_ecospace_scenarios(xmldoc)
+
+  #load driver node
+  tab=get_tables_from_name(xmldoc,"EcospaceScenarioHabitat")[[1]]
+
+  #load data table
+  df=table_to_df(tab)
+  df=df[df$ScenarioID==scenarios$ScenarioID[scenarios$ScenarioName==scenario_name],]
+
+  #create list of drivers, then parse values for each driver
+  l_maps=list()
+  for(i in 1:nrow(df))
+  {
+    map=list()
+    map$name=df$HabitatName[i]
+    map$id=df$HabitatID[i]
+    map$values=extract_map_data(df$HabitatMap[i],basemap)
+    class(map)="EcospaceMap"
+    l_maps[[map$name]]=map
+  }
+  l_maps
+}
+
+#' Load static MPA maps from EIIXML
+#'
+#' @param xmldoc XML2 document.
+#' @param scenario_name Name of the scenario.
+#'
+#' @returns List of \code{EcospaceMap} objects, each containing one MPA.
+#' @export
+get_ecospace_mpamaps=function(xmldoc,scenario_name)
+{
+  #load basemap as only data for in-AOI cells are stored
+  basemap=get_ecospace_basemap(xmldoc, scenario_name)
+
+  #load scenario information
+  scenarios=get_ecospace_scenarios(xmldoc)
+
+  #load driver node
+  tab=get_tables_from_name(xmldoc,"EcospaceScenarioMPA")[[1]]
+
+  #load data table
+  df=table_to_df(tab)
+  df=df[df$ScenarioID==scenarios$ScenarioID[scenarios$ScenarioName==scenario_name],]
+
+  #create list of MPAs, then parse spatial data for each
+  l_maps=list()
+  for(i in 1:nrow(df))
+  {
+    map=list()
+    map$name=df$MPAname[i]
+    map$id=df$MPAID[i]
+    map$months=df$MPAmonth[i]
+    map$values=extract_map_data(df$MPAMap[i],basemap,mpa=T)
+    if(is.null(map$values)) {map$values=basemap}
+    class(map)="EcospaceMap"
+    l_maps[[map$name]]=map
+  }
+  l_maps
+}
 
 
 #TODO
-#habitats, MPAs
 #load Ecospace data into model
 #factor for static Ecospace maps
 #deal with Ecosim scenarios
--- tests/testthat/test-read_xml_ecospace.R
+++ tests/testthat/test-read_xml_ecospace.R
@@ -5,3 +5,48 @@ test_that("Loading Ecospace scenarios works", {
   expect_equal(scenarios$ScenarioName,c("BayOfAnchovies", "New Ecospace scenario"))
   expect_equal(scenarios$ScenarioID,c(1,2))
 })
+
+test_that("Loading Ecospace depth map works", {
+  xmlfile=paste0(system.file('extdata', package = 'ecocx'),"/anchovy_bay_ecospace_ex.eiixml")
+  xmldoc=read_eiixml(xmlfile)
+  scenarios=get_ecospace_scenarios(xmldoc)
+  depth_map=get_ecospace_depthmap(xmldoc,scenarios$ScenarioName[1])
+  expect_equal(dim(depth_map$values),c(20,20))
+  expect_equal(depth_map$values[12,12],83)
+})
+
+test_that("Loading Ecospace base map works", {
+  xmlfile=paste0(system.file('extdata', package = 'ecocx'),"/anchovy_bay_ecospace_ex.eiixml")
+  xmldoc=read_eiixml(xmlfile)
+  scenarios=get_ecospace_scenarios(xmldoc)
+  base_map=get_ecospace_basemap(xmldoc,scenarios$ScenarioName[1])
+  expect_equal(dim(base_map$values),c(20,20))
+  expect_equal(sum(!is.na(base_map$values[8,])),17)
+})
+
+test_that("Loading Ecospace environemntal maps works", {
+  xmlfile=paste0(system.file('extdata', package = 'ecocx'),"/anchovy_bay_ecospace_ex.eiixml")
+  xmldoc=read_eiixml(xmlfile)
+  scenarios=get_ecospace_scenarios(xmldoc)
+  envmaps=get_ecospace_envmaps(xmldoc,scenarios$ScenarioName[1])
+  expect_equal(names(envmaps),c("Temperature","Distance from coast"))
+  expect_equal(envmaps$`Distance from coast`$values[8,6],80.04280)
+})
+
+test_that("Loading Ecospace habitat maps works", {
+  xmlfile=paste0(system.file('extdata', package = 'ecocx'),"/anchovy_bay_ecospace_ex.eiixml")
+  xmldoc=read_eiixml(xmlfile)
+  scenarios=get_ecospace_scenarios(xmldoc)
+  maps=get_ecospace_habmaps(xmldoc,scenarios$ScenarioName[1])
+  expect_equal(names(maps),c("All","Coastal","Sand","Rocky","Deep"))
+  expect_equal(round(maps$Rocky$values[12,15],4),0.4444)
+})
+
+test_that("Loading Ecospace MPA maps works", {
+  xmlfile=paste0(system.file('extdata', package = 'ecocx'),"/anchovy_bay_ecospace_ex.eiixml")
+  xmldoc=read_eiixml(xmlfile)
+  scenarios=get_ecospace_scenarios(xmldoc)
+  maps=get_ecospace_mpamaps(xmldoc,scenarios$ScenarioName[1])
+  expect_equal(dim(maps[[1]]$values),c(20,20))
+  expect_equal(rowSums(maps[[2]]$values[10:13,],na.rm=T),c(0,2,3,3))
+})
